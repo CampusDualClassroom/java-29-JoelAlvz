43abbc8dcb7dff02ed1ff9294d01933f3f83fa28
Joel Álvarez Rodriguez <joel@joel>
2024-10-02 22:22:48 +0200

    Ejercicio 29 resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,12 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
+        try {
 
+            throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+
+            System.out.println("Excepción: " + e.getMessage());
+        }
     }
 }
